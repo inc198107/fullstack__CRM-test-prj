79aa7a240b015dcb36ee86411432ef7e868130d4
dbelchenko <musicoverg@gmail.com>
2021-01-31 16:24:27 +0200

    add models add db conection

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const app = express();
+const keys = require('./config/keys');
 
-const authRoutes = require('./routes/auth');
-const analyticsRoutes = require('./routes/analytics');
-const categoryRoutes = require('./routes/category');
-const ordersRoutes = require('./routes/orders');
-const positionRoutes = require('./routes/position');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/analytics', analyticsRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/orders', ordersRoutes);
-app.use('/api/position', positionRoutes);
+mongoose
+  .connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const ordersRoutes = require("./routes/orders");
+const positionRoutes = require("./routes/position");
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/position", positionRoutes);
 
 module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://MySuperadmin:qwedf1981@meandb.atbkh.mongodb.net/mean-test?retryWrites=true&w=majority',
+}
