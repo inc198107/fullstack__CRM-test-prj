410c524b9e6786158382a88e061e181648066b79
dbelchenko <musicoverg@gmail.com>
2021-03-21 18:27:15 +0200

    add passport middleware

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const keys = require('./config/keys');
-
+const passport = require('passport');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -26,6 +26,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/analytics", analyticsRoutes);
